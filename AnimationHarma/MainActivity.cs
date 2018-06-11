38ce5656cd0ce2eccd0346533a9b6852eb593062
tonisharma <tonis.harma@tptlive.ee>
2018-06-11 09:25:05 +0300

    Animation
    Animation app made

---
+++ AnimationHarma/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace AnimationHarma
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+        }
+
+        public override void OnWindowFocusChanged(bool hasFocus)
+        {
+            if (hasFocus)
+            {
+                ImageView imageView = FindViewById<ImageView>(Resource.Id.imageView1);
+                AnimationDrawable animation = (AnimationDrawable)imageView.Drawable;
+                animation.Start();
+            }
+        }
+    }
+}
+
---
+++ AnimationHarma/obj/Debug/android/src/md5ae3d82535f9c925ec511859633c36849/MainActivity.java
@@ -0,0 +1,56 @@
+package md5ae3d82535f9c925ec511859633c36849;
+
+
+public class MainActivity
+	extends android.support.v7.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onWindowFocusChanged:(Z)V:GetOnWindowFocusChanged_ZHandler\n" +
+			"";
+		mono.android.Runtime.register ("AnimationHarma.MainActivity, AnimationHarma", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("AnimationHarma.MainActivity, AnimationHarma", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onWindowFocusChanged (boolean p0)
+	{
+		n_onWindowFocusChanged (p0);
+	}
+
+	private native void n_onWindowFocusChanged (boolean p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
